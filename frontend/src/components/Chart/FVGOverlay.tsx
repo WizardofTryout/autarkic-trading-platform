0d8e168e746170e9a3502cc318aa9cd081317787
Matthias Köhler <matthias.koehler.ms@edu.triagon-academy.com>
2025-12-05 17:50:03 +0100

    fix(chart): Resolve FVGZone import error using type-only import

--- frontend/src/components/Chart/FVGOverlay.tsx
+++ frontend/src/components/Chart/FVGOverlay.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import { FVGZone } from '../../utils/fvgDetection';
+import type { FVGZone } from '../../utils/fvgDetection';
 
 interface FVGOverlayProps {
     fvgZones: FVGZone[];
